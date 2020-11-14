e630263556f823c4f7701e1f3140a39f9868a2ec
dpbac <dani.barretto15@gmail.com>
2020-11-14 16:05:30 +0100

    refactor: Relocate clean_data function

--- train.py
+++ train.py
@@ -16,14 +16,6 @@ from azureml.data.dataset_factory import TabularDatasetFactory
 
 ds = TabularDatasetFactory.from_delimited_files(path = "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv")
 
-x, y = clean_data(ds)
-
-# TODO: Split data into train and test sets.
-# I'm using stratify since the data is a bit imbalanced 
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.30, random_state=123, stratify = y)
-
-run = Run.get_context()
-
 def clean_data(data):
     # Dict for cleaning data
     months = {"jan":1, "feb":2, "mar":3, "apr":4, "may":5, "jun":6, "jul":7, "aug":8, "sep":9, "oct":10, "nov":11, "dec":12}
@@ -49,6 +41,15 @@ def clean_data(data):
     x_df["poutcome"] = x_df.poutcome.apply(lambda s: 1 if s == "success" else 0)
 
     y_df = x_df.pop("y").apply(lambda s: 1 if s == "yes" else 0)
+    
+x, y = clean_data(ds)
+
+# TODO: Split data into train and test sets.
+# I'm using stratify since the data is a bit imbalanced 
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.30, random_state=123, stratify = y)
+
+run = Run.get_context()
+
     
 
 def main():
