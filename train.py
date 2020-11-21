3d260670a0ac335096e3107c10423b4c532cc392
dpbac <dani.barretto15@gmail.com>
2020-11-21 16:13:19 +0100

    refactor: refactor code to correct problems when running

--- train.py
+++ train.py
@@ -10,17 +10,16 @@ import pandas as pd
 from azureml.core.run import Run
 from azureml.data.dataset_factory import TabularDatasetFactory
 
-# TODO: Create TabularDataset using TabularDatasetFactory
-# Data is located at:
-# "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
+ds = TabularDatasetFactory.from_delimited_files(path="https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv")
 
-ds = TabularDatasetFactory.from_delimited_files(path = "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv")
+run = Run.get_context()
 
 def clean_data(data):
     # Dict for cleaning data
     months = {"jan":1, "feb":2, "mar":3, "apr":4, "may":5, "jun":6, "jul":7, "aug":8, "sep":9, "oct":10, "nov":11, "dec":12}
     weekdays = {"mon":1, "tue":2, "wed":3, "thu":4, "fri":5, "sat":6, "sun":7}
-
+    
+    
     # Clean and one hot encode data
     x_df = data.to_pandas_dataframe().dropna()
     jobs = pd.get_dummies(x_df.job, prefix="job")
@@ -42,16 +41,8 @@ def clean_data(data):
 
     y_df = x_df.pop("y").apply(lambda s: 1 if s == "yes" else 0)
     
-x, y = clean_data(ds)
-
-# TODO: Split data into train and test sets.
-# I'm using stratify since the data is a bit imbalanced 
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.30, random_state=123, stratify = y)
-
-run = Run.get_context()
-
+    return x_df,y_df
     
-
 def main():
     # Add arguments to script
     parser = argparse.ArgumentParser()
@@ -63,11 +54,27 @@ def main():
 
     run.log("Regularization Strength:", np.float(args.C))
     run.log("Max iterations:", np.int(args.max_iter))
+    
+    x, y = clean_data(ds)
+
+    # I'm using stratify since the data is a bit imbalanced 
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.30, random_state=123, stratify = y)
+
 
     model = LogisticRegression(C=args.C, max_iter=args.max_iter).fit(x_train, y_train)
 
     accuracy = model.score(x_test, y_test)
+    
+    os.makedirs('outputs',exist_ok = True)
+    
+    joblib.dump(model,'outputs/model.joblib')
+    
+    
     run.log("Accuracy", np.float(accuracy))
+    
+    
+    
+    
 
 if __name__ == '__main__':
     main()
